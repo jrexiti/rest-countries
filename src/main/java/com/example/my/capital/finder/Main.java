885397bc139f02a12fb3bd56d4adc45bdefb9eae
jrexiti <jay.rexiti@gmail.com>
2023-07-25 11:45:53 -0400

    adding user input to api

--- src/main/java/com/example/my/capital/finder/Main.java
+++ src/main/java/com/example/my/capital/finder/Main.java
@@ -1,25 +1,39 @@
 package com.example.my.capital.finder;
 
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Scanner;
+
+import javax.management.RuntimeErrorException;
+
+import org.json.simple.parser.JSONParser;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 
         Scanner scnr = new Scanner(System.in);
         String countryName;
+        String api = "https://restcountries.com/v3.1/name/";
 
         while (true) {
-            System.out.println("Please enter a country or enter Quit to exit program.");
+            System.out.println("please enter a country name or enter 'quit' to exit program.");
             countryName = scnr.nextLine();
 
-            if (countryName.equalsIgnoreCase("exit")) {
-                System.out.println("Exiting program....");
+            if (countryName.equalsIgnoreCase("quit")) {
+                System.out.println("exiting program....");
                 break;
             }
 
+            try {
+                URL url = new URL(api + countryName);
+                System.out.println(url);
+
+            } finally {
+
+            }
+
         }
-
     }
-
 }
