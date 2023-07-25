73417576502763cb5fd4e2e0a46015a329639915
jrexiti <jay.rexiti@gmail.com>
2023-07-25 12:30:19 -0400

    added logic to get country from user and pass it to api

--- src/main/java/com/example/my/capital/finder/Main.java
+++ src/main/java/com/example/my/capital/finder/Main.java
@@ -1,8 +1,33 @@
 package com.example.my.capital.finder;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String url = "https://restcountries.com/v3.1/name/";
+
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("please enter an country or enter 'exit' to exit the program.");
+
+        String userInput = scnr.nextLine();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest req = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url + userInput))
+                .build();
+
+        HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
 
     }
 
