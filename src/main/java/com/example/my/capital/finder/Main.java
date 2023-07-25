9adc845d209a4864cf47e5d99323b7796ca20ca6
jrexiti <jay.rexiti@gmail.com>
2023-07-25 11:49:26 -0400

    added status code test

--- src/main/java/com/example/my/capital/finder/Main.java
+++ src/main/java/com/example/my/capital/finder/Main.java
@@ -1,17 +1,19 @@
 package com.example.my.capital.finder;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
 import javax.management.RuntimeErrorException;
+import javax.net.ssl.HttpsURLConnection;
 
 import org.json.simple.parser.JSONParser;
 
 public class Main {
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws IOException {
 
         Scanner scnr = new Scanner(System.in);
         String countryName;
@@ -29,6 +31,13 @@ public class Main {
             try {
                 URL url = new URL(api + countryName);
                 System.out.println(url);
+
+                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+
+                int statusCode = conn.getResponseCode();
+
+                System.out.println(statusCode);
 
             } finally {
 
